10eb43c4fbe06b5d232563b53e666704d0acfb4f
Cédric Clément <cclem054@uottawa.ca>
2019-11-16 17:58:25 -0500

    Tokenized results form catalog manager

--- Detection_Spam/src/CatalogManager.java
+++ Detection_Spam/src/CatalogManager.java
@@ -1,14 +1,14 @@
+import java.io.IOException;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
-import java.nio.file.Path;
-import java.util.ArrayList;
 import java.util.stream.Collectors;
-import java.util.stream.*;
-//import java.nio.charset;
+import java.util.StringTokenizer;
+
 public class CatalogManager {
     private static String PATH_HAM = "./src/data/ham-and-spam-dataset/ham/";
     private static String PATH_SPAM = "./src/data/ham-and-spam-dataset/spam/";
@@ -21,10 +21,21 @@ public class CatalogManager {
         this.addDirectory("spam", PATH_SPAM);
     }
 
+    /**
+     * 
+     * @param key
+     * @param path
+     */
     public void addDirectory(String key, String path) {
         this.fileDirectories.put(key, path);
     }
 
+    /**
+     * Returns a hashmap of the filecontents.
+     * @param directory
+     * @return
+     * @throws IOException
+     */
     public HashMap<String, List<String>> GetContent(String directory) throws IOException{
         String path = this.fileDirectories.get(directory);
         if(path == null)
@@ -32,35 +43,29 @@ public class CatalogManager {
 
         Path p = Paths.get(path);
         
-        HashMap<String, List<String>> contents = new HashMap<String, List<String>>();
-//        Files.list(p)
-//        		.forEach(System.out::println);
-        
-        
         ArrayList<Path> paths = Files.list(p)
         		.collect(Collectors.toCollection(ArrayList::new));
         		
-        int readFiles = 0;
-        int skippedFiles = 0;
+        HashMap<String, List<String>> contents = new HashMap<String, List<String>>();
         for(Path f : paths) {
         	try {
         		List<String> lines = Files.readAllLines(f, Charset.forName("Cp1252"));
-//        		System.out.println("Read file!");
-//        		readFiles++;
+        		List<String> allTokens = new ArrayList<String>();
+        		for(String line : lines) {
+        			StringTokenizer tokens = new StringTokenizer(line);
+        			while(tokens.hasMoreTokens()) {
+        				allTokens.add(tokens.nextToken());
+        			}
+        		}
+//        		lines.sp
         		String name = f.getFileName().toString();
-        		contents.put(name, lines);
+        		contents.put(name, allTokens);
         	}catch(Exception e) {
-//        		System.out.println("Skipped file!");
-//        		skippedFiles++;
         	}
-        	
-        	
-        	
-//        	lines.forEach(System.out::println);
         	
         }
         return contents;
 
     }
-
+    
 }
--- Detection_Spam/src/SpamDetector.java
+++ Detection_Spam/src/SpamDetector.java
@@ -1,23 +1,13 @@
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.List;
 import java.io.IOException;
 
 public class SpamDetector {
-    public static void main(String[] args) throws IOException{
-        ListFiles();
-    }
     
-    public static void ListFiles() throws IOException{
+	public static void main(String[] args) throws IOException{
         CatalogManager manager = new CatalogManager();
         HashMap<String, List<String>> spam = manager.GetContent("spam");
         HashMap<String, List<String>> ham  = manager.GetContent("ham");
         System.out.println("Hello World");
-        
-        
     }
-
-
-
 }
