b09f0e54a93a4b13c5479dbdda5a4ae6b94082b8
Cédric Clément <cclem054@uottawa.ca>
2019-11-16 18:47:24 -0500

    Example of how to use stemmer

--- Detection_Spam/src/SpamDetector.java
+++ Detection_Spam/src/SpamDetector.java
@@ -10,6 +10,14 @@ public class SpamDetector {
         HashMap<String, List<String>> ham  = manager.GetContent("ham");
         HashMap<String, List<String>> stopwords = manager.GetContent("stopwords");
         List<String> words = stopwords.get("english.txt");
+        List<String> sample_ham = ham.get("2186.9dc59321a95e53d5e0ebaf3524858913");
+        
+        PorterStemmer stemmer = new PorterStemmer();
+        for(String s : sample_ham) {
+        	String stemmed = stemmer.stemWord(s);
+        	System.out.println(s + " " +stemmed + " ");
+        }
+        
         System.out.println("Hello World");
     }
 }
