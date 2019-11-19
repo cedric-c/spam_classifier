77db3675367825b2f8f0a17509b2220dae558fa4
minoucatou <minoucatou@gmail.com>
2019-11-19 11:29:40 -0500

    arranger bug dans fonction getPriors()

--- Detection_Spam/src/main.java
+++ Detection_Spam/src/main.java
@@ -133,29 +133,6 @@ public class main {
         
         System.out.println("classifier_Ham_Test: " + classifier_Ham_Test.size());
         System.out.println("classifier_Spam_Test: " + classifier_Spam_Test.size());
-        
-        
-		/*TESTER fonction stopWords() 
-		ArrayList<String> cars = new ArrayList<String>();
-	    cars.add("Ford");
-	    cars.add("started");
-	    cars.add("day");
-	    dictionnaire_Ham.put("automobile", cars);
-	    
-	    cars = new ArrayList<String>(); //créer nouvelle liste
-		cars.add("September");
-	    cars.add("yoo");
-	    cars.add("started");
-	    cars.add("course");
-	    dictionnaire_Ham.put("random", cars);
-	    
-	    System.out.println("Dictionnaire avant: " + dictionnaire_Ham); //avant
-	    a.traitementDeDonnees(dictionnaire_Ham, invertedIndex_Ham); 
-	    System.out.println("Dictionnaire stemmed: " + dictionnaire_Ham); //après
-	    
-	    System.out.println("");
-	    System.out.println("Inverted Index: " + invertedIndex_Ham);
-	    */
 	    	
 	}	
 }
